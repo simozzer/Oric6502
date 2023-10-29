bebfd1ab2cd77f867e8a01bd348d508091b0df19
simozzer <SimonMoscrop@hotmail.com>
2023-10-29 06:45:48 +0000

    fix bug in maze_procs.s

--- maze_procs.s
+++ maze_procs.s
@@ -4,7 +4,7 @@
 ;$7E is left of maze
 ;$7F is top position of maze
 ;$80 is temporary store for byte 
-    lda #$10 ; left
+    lda #$0E ; left
     sta $7E
     lda #01 ; top
     sta $7F
@@ -17,34 +17,18 @@
     lda mazeRowLookupTableHi,Y     ; lookup hi byte for row value and store
     sta getTheByte+2
 
-; find the correct byte from that row by dividing by 8 ;;TODO: Faster with lookup
-;    LDA $7E ; column
-;    LDX #0
-;    SEC
-;:Divide		
-;    INX
-;    SBC #$08
-;    BCS Divide
-;    ; X should now contain the correct column for the byte
-;    DEX ; !Experiment
-
+; find the correct byte from that row by dividing col b y 8
     ldx $7E
     lda divideBy8Table,x
-
+    tax
 
 :getTheByte
     lda $ffff,X
     sta $80 ; $80 should now contain the maze byte for the column and row
 
 ; Get the remainder of the above divison to find the
-; correct BIT for the maze wall  ;; TODO: Faster with lookup
+; correct BIT for the maze wall  ; Faster with lookup
 
-;    LDA $7E  ; column
-;    SEC
-;Modulus:	
-;    SBC #$08
-;    BCS Modulus
-;    ADC #$08
     ldx $7E
     lda mod8Table,X
 
@@ -52,11 +36,10 @@
     lda reverseBitmaskTable,X
 
     ; Logical AND with accumulator AND $80 should tell us if bit is set (i.e there is a wall)
-
     and $80
 
     ;; if accumulator is non zero there is a wall
-    bne nowall
+    beq nowall
     lda #$01
     rts
 :nowall
