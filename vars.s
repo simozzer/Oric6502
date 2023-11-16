c5c3ca524bcc51f55fca7ee7ff1afb6879d75038
simozzer <SimonMoscrop@hotmail.com>
2023-11-16 00:49:14 +0000

    Change from .byt to .dsb for zero page definitions

--- vars.s
+++ vars.s
@@ -4,68 +4,68 @@
 
 _zp_start_
 
-_plot_ch_x .byt 1
-_plot_ch_y .byt 1
-_plot_ascii .byt 1
+_plot_ch_x .dsb 1
+_plot_ch_y .dsb 1
+_plot_ascii .dsb 1
 _line_start
-_line_start_lo .byt 1
-_line_start_hi .byt 1
+_line_start_lo .dsb 1
+_line_start_hi .dsb 1
 
 _copy_mem_src
-_copy_mem_src_lo .byt 1
-_copy_mem_src_hi .byt 1
+_copy_mem_src_lo .dsb 1
+_copy_mem_src_hi .dsb 1
 _copy_mem_dest
-_copy_mem_dest_lo .byt 1
-_copy_mem_dest_hi .byt 1
+_copy_mem_dest_lo .dsb 1
+_copy_mem_dest_hi .dsb 1
 _copy_mem_count
-_copy_mem_count_lo .byt 1
-_copy_mem_count_hi .byt 1
+_copy_mem_count_lo .dsb 1
+_copy_mem_count_hi .dsb 1
 
-_maze_left .byt 1
-_maze_top .byt 1
-_maze_byte .byt 1
-_bits_to_process .byt 1
-_maze_bitmask .byt 1
-_maze_x_tmp .byt 1
-_maze_y_tmp .byt 1
+_maze_left .dsb 1
+_maze_top .dsb 1
+_maze_byte .dsb 1
+_bits_to_process .dsb 1
+_maze_bitmask .dsb 1
+_maze_x_tmp .dsb 1
+_maze_y_tmp .dsb 1
 _maze_line_start
-_maze_line_start_lo .byt 1
-_maze_line_start_hi .byt 1
+_maze_line_start_lo .dsb 1
+_maze_line_start_hi .dsb 1
 
-_maze_right .byt 1
+_maze_right .dsb 1
 
-_player1_x .byt 1
-_player1_y .byt 1
-_player1_direction .byt 1
-_player_status .byt 1
-_player1_maze_x .byt 1
-_player1_maze_y .byt 1
+_player1_x .dsb 1
+_player1_y .dsb 1
+_player1_direction .dsb 1
+_player_status .dsb 1
+_player1_maze_x .dsb 1
+_player1_maze_y .dsb 1
 
-_player2_x .byt 1
-_player2_y .byt 1
-_player2_direction .byt 1
-_player2_maze_x .byt 1
-_player2_maze_y .byt 1
+_player2_x .dsb 1
+_player2_y .dsb 1
+_player2_direction .dsb 1
+_player2_maze_x .dsb 1
+_player2_maze_y .dsb 1
 
 
-_screen_render_right .byt 1
-_screen_render_bottom .byt 1
-_maze_render_offset_x .byt 1
-_screen_render_x_wrap .byt 1 ; the value x must hit to go to the previous line
-_screen_render_y_wrap .byt 1 ; the value y must be for render complete
+_screen_render_right .dsb 1
+_screen_render_bottom .dsb 1
+_maze_render_offset_x .dsb 1
+_screen_render_x_wrap .dsb 1 ; the value x must hit to go to the previous line
+_screen_render_y_wrap .dsb 1 ; the value y must be for render complete
 
-_scroll_left_maze_x_threshold .byt 1
-_scroll_right_maze_x_threshold .byt 1
-_scroll_right_max_maze_x .byt 1
-_scroll_up_maze_y_threshold .byt 1
-_scroll_down_maze_y_threshold .byt 1
-_scroll_down_max_maze_y .byt 1
+_scroll_left_maze_x_threshold .dsb 1
+_scroll_right_maze_x_threshold .dsb 1
+_scroll_right_max_maze_x .dsb 1
+_scroll_up_maze_y_threshold .dsb 1
+_scroll_down_maze_y_threshold .dsb 1
+_scroll_down_max_maze_y .dsb 1
 
-_display_mode .byt 1
+_display_mode .dsb 1
 
-_possible_directions .byt 1
+_possible_directions .dsb 1
 
-_player_animation_index .byt 1
+_player_animation_index .dsb 1
 
 _zp_end_
 
