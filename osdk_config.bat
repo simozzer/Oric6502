32014eecf50885f1ff4677e4927627f6e019eb4c
simozzer <SimonMoscrop@hotmail.com>
2024-04-02 04:59:19 +0100

    Change start address to $1400

--- osdk_config.bat
+++ osdk_config.bat
@@ -3,7 +3,7 @@
 ::
 :: Set the build paremeters
 ::
-SET OSDKADDR=$2000
+SET OSDKADDR=$1400
 SET OSDKNAME=Scroll
 SET OSDKLINK=-B
 SET OSDKFILE=vars defines ./SoundEffects/sound_effect_defines main maze_procs screen_render trail_memory ./Keyboard/whole_keyboard ./Keyboard/keys_to_scan joystick_ijk collision_info ./Keyboard/keyboard_mapper setScreenMetrics movement ./SoundEffects/sound_effects computer_player utils ./ScreenEffects/screen_effects rom_independent_sound tracker_interrupt spranimation spr tracker_data messages lookup ./Keyboard/keyboard_mapper_data maze_data   
